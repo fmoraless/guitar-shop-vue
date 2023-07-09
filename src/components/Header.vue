8169bd86f3ac305115e4075b927f2f16c602b970
fmoraless <franticzero@gmail.com>
2023-07-09 15:53:26 -0400

    feat(shopping-cart): calculate total cart

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { computed } from 'vue'
+
 const props = defineProps({
     carrito: {
         type: Array,
@@ -9,7 +11,15 @@ const props = defineProps({
         required: true,
     },
 })
+
 defineEmits(['incrementar-cantidad', 'decrementar-cantidad', 'agregar-carrito'])
+
+const totalPagar = computed(() => {
+    return props.carrito.reduce(
+        (total, producto) => total + producto.cantidad * producto.precio,
+        0
+    )
+})
 </script>
 
 <template>
@@ -114,7 +124,8 @@ defineEmits(['incrementar-cantidad', 'decrementar-cantidad', 'agregar-carrito'])
                             </table>
 
                             <p class="text-end">
-                                Total pagar: <span class="fw-bold">$899</span>
+                                Total pagar:
+                                <span class="fw-bold">${{ totalPagar }}</span>
                             </p>
                             <button class="btn btn-dark w-100 mt-3 p-2">
                                 Vaciar Carrito
